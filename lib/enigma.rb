1a17789ed06de779337cb4ed6d313993741dd133
Croix Kimmel <croixkimmel@gmail.com>
2021-11-10 21:20:27 -0700

    Update enigma - passing encrypt, decrypt tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -35,35 +35,107 @@ class Enigma
 
     # split message into array
     message_array = message.split(//)
-
-    # require "pry"; binding.pry
-
     encrypted_array = []
-    # if, elsif, etc, for index position
-    for i in 1..(message_array.length)
-      if i%4 == 1   # a
-        index = (i + a_shift)%27
-        encrypted_array << @alphabet[index]
-      elsif i%4 == 2 # b
-        index = (i + b_shift)%27
-        encrypted_array << @alphabet[index]
-      elsif i%4 == 3 # c
-        index = (i + c_shift)%27
-        encrypted_array << @alphabet[index]
-      elsif i%4 == 0 # d
-        index = (i + d_shift)%27
-        encrypted_array << @alphabet[index]
+
+    for i in 0..(message_array.length - 1)
+      if i%4 == 0   # a
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (a_shift + alphabet_index)%27
+
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 1 # b
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (b_shift + alphabet_index)%27
+
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 2 # c
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (c_shift + alphabet_index)%27
+
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 3 # d
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (d_shift + alphabet_index)%27
+
+        encrypted_array << @alphabet[new_index]
       end
     end
 
-    require "pry"; binding.pry
     # return hash - 3 keys (encryption, key, date)
-    encrypted_array
+    encrypted_hash = {}
+    encrypted_hash[:date] = date
+    encrypted_hash[:encryption] = encrypted_array.join
+    encrypted_hash[:key] = key
+    encrypted_hash
   end
 
 
-  def decrypt(ciphertext, key, date)
+  def decrypt(ciphertext, key = 0, date = Date)
+    ciphertext = ciphertext.downcase
 
+    if key == 0
+      key = rand(99999)
+    end
+
+    #make key into 5 character string
+    key_string = key.to_s.rjust(5)
+
+    # produce (4) two digit int keys
+    a_key = key_string[0, 2].to_i
+    b_key = key_string[1, 2].to_i
+    c_key = key_string[2, 2].to_i
+    d_key = key_string[3, 2].to_i
+
+    # square date - only keep last 4 digits
+    date_square_4 = ((date.to_i)**2).to_s[-4,4]
+
+    a_shift = date_square_4[0].to_i + a_key
+    b_shift = date_square_4[1].to_i + b_key
+    c_shift = date_square_4[2].to_i + c_key
+    d_shift = date_square_4[3].to_i + d_key
+
+    # split message into array
+    message_array = ciphertext.split(//)
+    encrypted_array = []
+
+    for i in 0..(message_array.length - 1)
+      if i%4 == 0   # a
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (-a_shift + alphabet_index)%27
+        if new_index < 0
+          new_index = 27 + new_index
+        end
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 1 # b
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (-b_shift + alphabet_index)%27
+        if new_index < 0
+          new_index = 27 + new_index
+        end
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 2 # c
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (-c_shift + alphabet_index)%27
+        if new_index < 0
+          new_index = 27 + new_index
+        end
+        encrypted_array << @alphabet[new_index]
+      elsif i%4 == 3 # d
+        alphabet_index = @alphabet.index(message_array[i])
+        new_index = (-d_shift + alphabet_index)%27
+        if new_index < 0
+          new_index = 27 + new_index
+        end
+        encrypted_array << @alphabet[new_index]
+      end
+    end
+
+    # return hash - 3 keys (encryption, key, date)
+    encrypted_hash = {}
+    encrypted_hash[:date] = date
+    encrypted_hash[:decryption] = encrypted_array.join
+    encrypted_hash[:key] = key
+    encrypted_hash
   end
 
 end
