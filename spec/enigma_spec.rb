c76f3c65a2bbb1ff3f91805c9bd27e77d7fa5d13
Croix Kimmel <croixkimmel@gmail.com>
2021-11-10 19:55:30 -0700

    Update enigma_spec

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,6 +5,22 @@ RSpec.describe Enigma do
 
   it 'exists' do
     enigma = Enigma.new
+    expect(enigma).to be_instance_of(Enigma)
   end
+
+  it 'encrypt method' do
+    enigma = Enigma.new
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it 'decrypt method' do
+    enigma = Enigma.new
+    expected =  {decryption: "hello world", key: "02715", date: "040895"}
+
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+  end
+
 
 end
