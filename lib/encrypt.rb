fd2ba19abaf10c29ff7e064c3787e88ff48e2098
Croix Kimmel <croixkimmel@gmail.com>
2021-11-11 17:04:51 -0700

    Modify code to write to file - still need to modify to print message

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,13 +3,13 @@ require './lib/enigma'
 message_file = File.open('message.txt', 'r')
 read_message = message_file.read
 
+enigma = Enigma.new
 
 
+puts encrypted = enigma.encrypt(read_message, "02715", "040895")
 
+encrypted_file = File.open('encrypted.txt', 'w')
+encrypted_file.write(enigma.encrypt(read_message, "02715", "040895"))
 
 
 ARGV == ['message.txt', 'encrypted.txt']
-
-
-
-require "pry"; binding.pry
