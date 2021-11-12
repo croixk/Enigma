51ede2713dbae9073910315a0119c65eb11ccf7a
Croix Kimmel <croixkimmel@gmail.com>
2021-11-11 18:00:31 -0700

    Modify encrypt.rb to use ARGV, print out summary line

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,21 @@
 require './lib/enigma'
 
+arguments = ARGV
+
+key = "02715"
+date = "040895"
+
 message_file = File.open('message.txt', 'r')
 read_message = message_file.read
 
 enigma = Enigma.new
 
 
-puts encrypted = enigma.encrypt(read_message, "02715", "040895")
 
 encrypted_file = File.open('encrypted.txt', 'w')
-encrypted_file.write(enigma.encrypt(read_message, "02715", "040895"))
 
 
-ARGV == ['message.txt', 'encrypted.txt']
+
+encrypted_file.write(enigma.encrypt(read_message, key, date))
+
+puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
