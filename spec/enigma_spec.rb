cccc4d3305d72e53f2fa8f9632bb244e34356f7e
Croix Kimmel <croixkimmel@gmail.com>
2021-11-14 10:07:09 -0700

    Update enigma_spec for encrypted_character, decrypted_character methods

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,6 +9,19 @@ RSpec.describe Enigma do
     expect(enigma).to be_instance_of(Enigma)
   end
 
+  it 'encrypted_character' do
+    enigma = Enigma.new
+    expect(enigma.encrypted_character(1, ["a"], 0)).to eq("b")
+    expect(enigma.encrypted_character(1, ["?"], 0)).to eq("?")
+  end
+
+  it 'decrypted_character' do
+    enigma = Enigma.new
+    expect(enigma.decrypted_character(1, ["b"], 0)).to eq("a")
+    expect(enigma.decrypted_character(1, ["?"], 0)).to eq("?")
+  end
+
+
   it 'encrypt method' do
     enigma = Enigma.new
     expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
@@ -23,5 +36,7 @@ RSpec.describe Enigma do
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
 
+
+  ## add tests for uppercase letters, special characters - all edge cases
 
 end
